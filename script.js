ac2392493237371ca3ae8e6f3d0c2458b443ee2d
Gloria <gloghe@yahoo.it>
2022-05-24 14:00:45 +0200

    immagine bonus

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var miaApp = new Vue ({
     
         el:"#container",
         data: {
-            messaggio: "La classe 64 di Boolean è la migliore!!!"
+            messaggio: "La classe 64 di Boolean è la migliore!!!",
+            miaImmagine: "https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/681/original/Pittogramma-RGB-1080-BLUE-circle.png",
         }
     
 })
