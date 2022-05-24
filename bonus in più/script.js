f3d8faf6a7f6155383b7a7b9ac193bf94a745712
Gloria <gloghe@yahoo.it>
2022-05-24 16:49:13 +0200

    BONUS IMMAGINI ANIMALI

---
+++ bonus in più/script.js
@@ -0,0 +1,25 @@
+var miaPagina = new Vue ({
+    
+    el:"#container",
+    data: {
+        messaggio: "Scegli il disegno che preferisci:",
+        miaImmagine: "",
+    },
+    methods: {
+        scelta: function () {
+           let input = document.querySelector('input[name="animal"]:checked')
+           debugger;
+            switch (input.value) {
+            case "GATTI":
+                this.miaImmagine = "https://portalebambini.it/wp-content/uploads/2021/06/gatti-colorare-001-1-724x1024.jpg";  
+                break;
+            case "CANI":
+                this.miaImmagine = "https://st2.depositphotos.com/3903847/5567/v/450/depositphotos_55674625-stock-illustration-cute-puppy-dog.jpg";  
+                break;
+            case "TARTARUGHE":
+                this.miaImmagine = "https://portalebambini.it/wp-content/uploads/2021/06/tartaruge-colorare-001-1024x724.jpg";  
+                break;
+            }  
+        }
+    }
+})
